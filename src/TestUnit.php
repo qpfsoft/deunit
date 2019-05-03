cff5f735e63542504a710c20ae534cd1ac913aa1
qiun <qiun@163.com>
2019-05-03 17:13:53 +0800

    添加测试前后回调方法

--- src/TestUnit.php
+++ src/TestUnit.php
@@ -8,6 +8,22 @@ abstract class TestUnit
 {
     use TypeTrait;
     use ValueTrait;
+    
+    /**
+     * 测试开始回调方法
+     */
+    public function setUp()
+    {
+        
+    }
+    
+    /**
+     * 测试结束回调方法
+     */
+    public function tearDown()
+    {
+        
+    }
 
     /**
      * 运行单元测试
@@ -17,28 +33,35 @@ abstract class TestUnit
      */
     final public static function runTestUnit($throw = false)
     {
-        $reclass = new \ReflectionClass(static::class);
+        $object = new static();
+        $reclass = new \ReflectionClass($object);
         
+        // 公共方法
         $methods = $reclass->getMethods(\ReflectionMethod::IS_PUBLIC);
         
         $filter = ['runTestUnit']; // 'assert'
         
         $result = [];
         
+        $object->setUp();
+        
         /* @var $method \ReflectionMethod  */
         foreach ($methods as $method) {
             $name = $method->getName();
             
-            if (in_array($name, $filter)) {
+            // test前缀方法名
+            if (strncasecmp($name, 'test', 4) !== 0 || in_array($name, $filter)) {
                 continue;
             }
             
-            $result[$name] = $method->invoke(new static()) ? '__ok__' : '__error__';
+            $result[$name] = $method->invoke($object) ? '__ok__' : '__error__';
             
             if ($throw && $result[$name] == '__error__') {
                 throw new \Exception($name . ' Test failed');
             }
         }
+        
+        $object->tearDown();
         
         $assess = array_count_values($result);
         
